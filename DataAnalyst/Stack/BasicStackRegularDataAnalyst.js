ad19576f90954d9d318a5dc86564b07511d51013
Wang Sheng <wang.sheng.unique@gmail.com>
2018-12-14 00:01:35 +0900

    BasicStackRegularDataAnalyst

--- DataAnalyst/Stack/BasicStackRegularDataAnalyst.js
+++ DataAnalyst/Stack/BasicStackRegularDataAnalyst.js
@@ -1,11 +1,19 @@
 /**
- * Created by wangsheng on 30/6/14.
+ * this class analyze the input data of a basic stack chart
+ * @param input                 the input data of a basic stack chart
+ * @param xAxisDataAreaLength   the display area length of the data. this is used to determine if the space is enough for drawing
+ *                              a node for each single data point.
+ * @constructor
  */
 function BasicStackRegularDataAnalyst(input, xAxisDataAreaLength){
     this.input = input;
     this.xAxisDataAreaLength = xAxisDataAreaLength;
 }
 
+/**
+ * finds the min max value, and if the space is big enough for drawing a node for every single data point.
+ * @returns the result of the analysis.
+ */
 BasicStackRegularDataAnalyst.prototype.analyze = function(){
     //initialize the minY and maxY to be 0, so that if all other values are positive, the minY is set to be 0 no matter
     //what the smallest value is. If all other values are negative, the maxY is set to be 0 no matter what the biggest value
@@ -37,10 +45,10 @@ BasicStackRegularDataAnalyst.prototype.analyze = function(){
     var maxX = util.chooseBetween(this.input.start === undefined, 0, this.input.start) + (singleSeriesLength - 1) * this.input.interval;
 
     return  {
-    minX: this.input.start,
-    maxX: maxX,
-    minY: minY,
-    maxY: maxY,
-    isContinual: isContinual
+        minX: this.input.start,
+        maxX: maxX,
+        minY: minY,
+        maxY: maxY,
+        isContinual: isContinual
     };
 };
