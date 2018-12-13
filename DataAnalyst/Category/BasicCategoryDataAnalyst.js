d754342c414c549a28178175f567b6b9a51fbf6b
Wang Sheng <wang.sheng.unique@gmail.com>
2018-12-13 23:52:04 +0900

    clean up BasicCategoryDataAnalyst and add doc

--- DataAnalyst/Category/BasicCategoryDataAnalyst.js
+++ DataAnalyst/Category/BasicCategoryDataAnalyst.js
@@ -1,26 +1,31 @@
 /**
- * Created by wangsheng on 11/6/14.
+ * this class is used to find out the min max value in the data, for a category chart.
+ * @param input     input data of a basic category chart
+ * @constructor
  */
-
 function BasicCategoryDataAnalyst(input){
     this.input = input;
     this.results = null;
 }
 
+/**
+ * find out the min max value in the data. the analysis result is stored in this analyst instance as `this.result`.
+ * @returns {{minY: number, maxY: number, seriesNames: Array }}
+ */
 BasicCategoryDataAnalyst.prototype.analyze = function(){
     if(this.results !== null){
         return this.results;
     }
 
-    var minY = 0;
-    var maxY = 0;
+    let minY = 0;
+    let maxY = 0;
 
     //loop through different series
-    var series = this.input.series;
-    var seriesNames = [];
-    for(var i = 0; i < series.length; i++){
+    let series = this.input.series;
+    let seriesNames = [];
+    for(let i = 0; i < series.length; i++){
         seriesNames.push(series[i][0]);
-        var seriesData = series[i][1];
+        let seriesData = series[i][1];
 
         if(seriesData < minY){
             minY = seriesData;
