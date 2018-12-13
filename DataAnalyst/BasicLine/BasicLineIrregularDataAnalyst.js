c659809f34340a7a98ca64b99b949b57ff4aee19
Wang Sheng <wang.sheng.unique@gmail.com>
2018-12-13 23:05:19 +0900

    more comments and docs for basci line data analyzer

--- DataAnalyst/BasicLine/BasicLineIrregularDataAnalyst.js
+++ DataAnalyst/BasicLine/BasicLineIrregularDataAnalyst.js
@@ -1,11 +1,8 @@
 /**
- * Created by wangsheng on 31/5/14.
- */
-
-/**
  * this implementation analyzes the data for a basic line chart that has both linear data y axis and linear data x axis.
- * @param data
- * @param the length of x axis, excluding left and right padding.
+ * @param input                 the input that carries data belonging to different series.
+ * @param xAxisDataAreaLength   the area where we are going to display the data (using node), if the area is too small,
+ *                              we will display a continual line, otherwise we will display a node for each of the data point
  * @constructor
  */
 function BasicLineIrregularDataAnalyst(input, xAxisDataAreaLength){
@@ -13,6 +10,11 @@ function BasicLineIrregularDataAnalyst(input, xAxisDataAreaLength){
     this.xAxisDataAreaLength = xAxisDataAreaLength;
 }
 
+/**
+ * find the min max in all series' data, also find if we need to draw a continual line to represent the data,
+ * or do we need to draw each node for each data point, depending on how much space we have.
+ * @returns {{minX: number, maxX: number, minY: number, maxY: number, isContinual: (boolean)}}
+ */
 BasicLineIrregularDataAnalyst.prototype.analyze = function(){
     var series = this.input.series;
     //find the smallest & biggest value on x axis, and find the smallest & biggest value on y axis.
