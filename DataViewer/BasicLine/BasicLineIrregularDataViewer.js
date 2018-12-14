8d78b50ea982c154b5afd0de211d1876fd28affc
Wang Sheng <wang.sheng.unique@gmail.com>
2018-12-14 10:50:34 +0900

    add doc, import and es6 syntax to BasicLineIrregularDataViewer

--- DataViewer/BasicLine/BasicLineIrregularDataViewer.js
+++ DataViewer/BasicLine/BasicLineIrregularDataViewer.js
@@ -1,15 +1,17 @@
 /**
- * Created by wangsheng on 4/6/14.
- */
-
-/**
  * construct a basic line linear data.
- * @param data
- * @param xDrawInfo
- * @param yDrawInfo
+ * @param htmlContainer     the html container (html layer) is needed as we need to append the tooltip to it.
+ * @param svg               to which we will append the svg elements
+ * @param svgTrigger        tooltip trigger
+ * @param input             the data points
+ * @param xDrawInfo         the analysis of x axis, we need to draw the lines / nodes according to the axis (of course)
+ * @param yDrawInfo         the analysis of y axis
+ * @param isContinual       draw nodes / lines
  * @constructor
  */
-function BasicLineIrregularDataViewer(htmlContainer, svg, svgTrigger, input, xDrawInfo, yDrawInfo, isContinual) {
+import {TipControl} from "../Sub/Tip/TipControl";
+
+export function BasicLineIrregularDataViewer(htmlContainer, svg, svgTrigger, input, xDrawInfo, yDrawInfo, isContinual) {
     this.htmlContainer = htmlContainer;
     this.svg = svg;
     this.svgTrigger = svgTrigger;
@@ -23,29 +25,30 @@ function BasicLineIrregularDataViewer(htmlContainer, svg, svgTrigger, input, xDr
 
 /**
  * need not be called after analyze() because there is no such method in this class.
+ * draw the lines / nodes for data.
  */
 BasicLineIrregularDataViewer.prototype.draw = function () {
-    var tipControl = new TipControl(this.htmlContainer, 7, false);
+    let tipControl = new TipControl(this.htmlContainer, 7, false);
     tipControl.createTip();
 
     //loop through different series
-    var series = this.input.series;
-    var randomPicker = new RandomPicker();
+    let series = this.input.series;
+    let randomPicker = new RandomPicker();
 
-    var svgDrawGroup = draw.createGroup();
-    var svgTriggerGroup = draw.createGroup();
+    let svgDrawGroup = draw.createGroup();
+    let svgTriggerGroup = draw.createGroup();
 
-    for(var i = 0; i < series.length; i++) {
-        var singleSeriesName = series[i][0];
-        var singleSeriesData = series[i][1];
-        var nodes = this.analyzeSingleSeriesData(singleSeriesData);
+    for(let i = 0; i < series.length; i++) {
+        let singleSeriesName = series[i][0];
+        let singleSeriesData = series[i][1];
+        let nodes = this.analyzeSingleSeriesData(singleSeriesData);
 
-        var mcColor = randomPicker.pickSeriesColor();
-        var nodeShape = randomPicker.pickNodeShape();
+        let mcColor = randomPicker.pickSeriesColor();
+        let nodeShape = randomPicker.pickNodeShape();
         //I need to pass in the this.htmlContainer rather than a div that serves as a group because I need to
         //append the tip template to the dom in order to use its getClientBoundingRect method. And this.htmlContainer
         //is already inserted into the DOM.
-        var singleLineSeriesViewer = new SingleLineSeriesViewer(this.htmlContainer, svgDrawGroup, svgTriggerGroup, nodes, nodeShape, mcColor,
+        let singleLineSeriesViewer = new SingleLineSeriesViewer(this.htmlContainer, svgDrawGroup, svgTriggerGroup, nodes, nodeShape, mcColor,
             this.isContinual, false, singleSeriesName, tipControl, this.xDrawInfo, this.yDrawInfo);
         singleLineSeriesViewer.draw();
     }
@@ -58,15 +61,15 @@ BasicLineIrregularDataViewer.prototype.draw = function () {
 /**
  * returns an array that contains both pixel information and data information. The stride is 4 and the first 2 are pixel positions
  * while the last 2 are data information.
- * @param singleSeriesData
+ * @param singleSeriesData      the data of a single series
  * @returns {Array}
  */
 BasicLineIrregularDataViewer.prototype.analyzeSingleSeriesData = function(singleSeriesData){
-    var nodes = []; //reset it to empty array.
+    let nodes = []; //reset it to empty array.
 
-    for (var ii = 0; ii < singleSeriesData.length; ii++) {
-        var x = this.xDrawInfo.startPoint + (singleSeriesData[ii][0] - this.xDrawInfo.min) * this.xDrawInfo.pixelPerData;
-        var y = this.yDrawInfo.startPoint - (singleSeriesData[ii][1] - this.yDrawInfo.min) * this.yDrawInfo.pixelPerData;
+    for (let ii = 0; ii < singleSeriesData.length; ii++) {
+        let x = this.xDrawInfo.startPoint + (singleSeriesData[ii][0] - this.xDrawInfo.min) * this.xDrawInfo.pixelPerData;
+        let y = this.yDrawInfo.startPoint - (singleSeriesData[ii][1] - this.yDrawInfo.min) * this.yDrawInfo.pixelPerData;
 
         nodes.push(x);
         nodes.push(y);
