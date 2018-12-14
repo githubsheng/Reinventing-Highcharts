fbb16e3930c15d8ae03ad08d5219907f1bdf7368
Wang Sheng <wang.sheng.unique@gmail.com>
2018-12-14 14:26:37 +0900

    refine BasicCategoryDataViewer

--- DataViewer/Category/BasicCategoryDataViewer.js
+++ DataViewer/Category/BasicCategoryDataViewer.js
@@ -2,14 +2,19 @@
  * Created by wangsheng on 12/6/14.
  */
 
+import {RandomPicker} from "../../Util/RandomPicker";
+import {draw} from "../../Draw/Draw";
+import {TipControl} from "../Sub/Tip/TipControl";
+
 /**
+ * this class is used to draw category columns
  *
- * @param htmlContainer
- * @param svg
- * @param svgTrigger
- * @param input
- * @param xDrawInfo
- * @param yDrawInfo
+ * @param htmlContainer     the html container (html layer) is needed as we need to append the tooltip to it.
+ * @param svg               to which we will append the svg elements
+ * @param svgTrigger        tooltip trigger
+ * @param input             the data points
+ * @param xDrawInfo         the analysis of x axis, we need to draw the lines / nodes according to the axis (of course)
+ * @param yDrawInfo         the analysis of y axis
  * @constructor
  */
 export function BasicCategoryDataViewer(htmlContainer, svg, svgTrigger, input, xDrawInfo, yDrawInfo){
@@ -25,20 +30,24 @@ export function BasicCategoryDataViewer(htmlContainer, svg, svgTrigger, input, x
  * public method. called by the weaver to draw the columns, and also set proper event listeners on those columns
  */
 BasicCategoryDataViewer.prototype.draw = function(){
-    var random = new RandomPicker();
-    var svgGroup = draw.createGroup();
-    var svgTriggerGroup = draw.createGroup();
-    var tipControl = new TipControl(this.htmlContainer, 7, true);
+    let random = new RandomPicker();
+    let svgGroup = draw.createGroup();
+    let svgTriggerGroup = draw.createGroup();
+    let tipControl = new TipControl(this.htmlContainer, 7, true);
     tipControl.createTip();
 
-    for(var i = 0; i <  this.xDrawInfo.categoryBasePositions.length; i = i + 2){
+    for(let i = 0; i <  this.xDrawInfo.categoryBasePositions.length; i = i + 2){
         //in this case the nodes array will contain only one node. This is because a basic column series will only have one data.
-        var nodes = [this.xDrawInfo.categoryBasePositions[i]/*pixelX*/, this.xDrawInfo.categoryBasePositions[i+1],
-        0/*dataX, does not matter here*/, this.input.series[i/2][1]];
+        let nodes = [
+            this.xDrawInfo.categoryBasePositions[i]/*pixelX*/,
+            this.xDrawInfo.categoryBasePositions[i+1],
+            0/*dataX, does not matter here*/,
+            this.input.series[i/2][1]
+        ];
 
-        var seriesName = this.input.series[i/2][0];
+        let seriesName = this.input.series[i/2][0];
 
-        var scv = new SingleColumnViewer();
+        let scv = new SingleColumnViewer();
 
         scv.draw(nodes[0], nodes[1], this.xDrawInfo.columnWidth, seriesName, nodes[3], random.pickSeriesColor(), nodes, svgGroup, svgTriggerGroup,
             tipControl, this.htmlContainer, this.yDrawInfo);
