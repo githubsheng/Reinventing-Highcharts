359d26c3798e1519c81d1bfafe99aa1eef7605be
Wang Sheng <wang.sheng.unique@gmail.com>
2018-12-14 14:36:55 +0900

    refine the pie data viewer

--- DataViewer/Pie/BasicPieDataViewer.js
+++ DataViewer/Pie/BasicPieDataViewer.js
@@ -1,7 +1,25 @@
+import {RandomPicker} from "../../Util/RandomPicker";
+import {TipControl} from "../Sub/Tip/TipControl";
+import {draw} from "../../Draw/Draw";
+
 /**
- * Created by wangsheng on 4/7/14.
+ *
+ *
+ * this draws a pie and set up the tool tips
+ * @param htmlContainer                 the html container (html layer) is needed as we need to append the tooltip to it.
+ * @param svg                           to which we will append the svg elements
+ * @param svgTrigger                    tooltip trigger
+ * @param input                         the data points
+ * @param center                        the center of the pie
+ * @param dataDrawAreaX                 not sure how this is useful. remove if tested to be unused.
+ * @param dataDrawAreaY                 not sure how this is useful. remove if tested to be unused.
+ * @param radiusForLabel                radius of the invisible circle, along which we draw the labels
+ * @param radisuForPie                  radius of the pie circle
+ * @param radiusForConnectionLineTurn   radius of the invisible circle, along which we set the middle point of the connection line (cubic curve).
+ * @param total                         the sum of all series
+ * @constructor
  */
-function BasicPieDataViewer(htmlContainer, svg, svgTrigger, input, center, dataDrawAreaX, dataDrawAreaY, radiusForLabel, radisuForPie, radiusForConnectionLineTurn, total){
+export function BasicPieDataViewer(htmlContainer, svg, svgTrigger, input, center, dataDrawAreaX, dataDrawAreaY, radiusForLabel, radisuForPie, radiusForConnectionLineTurn, total){
     this.htmlContainer = htmlContainer;
     this.svg = svg;
     this.svgTrigger = svgTrigger;
@@ -15,30 +33,33 @@ function BasicPieDataViewer(htmlContainer, svg, svgTrigger, input, center, dataD
     this.total = total;
 }
 
+/**
+ * draw the pie, label,s and connection lines. set up the tool tips as well.
+ */
 BasicPieDataViewer.prototype.draw = function(){
-    var slices = this.analyze();
-    var random = new RandomPicker();
+    let slices = this.analyze();
+    let random = new RandomPicker();
 
-    var svgGroup = draw.createGroup();
-    var svgTriggerGroup = draw.createGroup();
+    let svgGroup = draw.createGroup();
+    let svgTriggerGroup = draw.createGroup();
 
-    var tipControl = new TipControl(this.htmlContainer, 0, true);
+    let tipControl = new TipControl(this.htmlContainer, 0, true);
     tipControl.createTip();
 
-    var labelSlotsController = new PieDataLabelSlotsController();
+    let labelSlotsController = new PieDataLabelSlotsController();
     labelSlotsController.generateEmptySlots(19/*label height*/, this.center, this.radiusForLabel, this.svg);
 
-    var leftConnectorsInfo = [];
-    var rightConnectorsInfo = [];
+    let leftConnectorsInfo = [];
+    let rightConnectorsInfo = [];
 
-    for(var i = 0; i < slices.length; i = i + 4){
-        var startAngle = slices[i];
-        var endAngle = slices[i+1];
-        var seriesName = slices[i+2];
-        var dataY = slices[i+3];
-        var spsv = new SinglePieSeriesViewer(svgGroup, this.htmlContainer, tipControl, svgTriggerGroup, startAngle,
+    for(let i = 0; i < slices.length; i = i + 4){
+        let startAngle = slices[i];
+        let endAngle = slices[i+1];
+        let seriesName = slices[i+2];
+        let dataY = slices[i+3];
+        let spsv = new SinglePieSeriesViewer(svgGroup, this.htmlContainer, tipControl, svgTriggerGroup, startAngle,
             endAngle, this.radiusForLabel, this.radiusForPie, this.radiusForConnectionLineTurn, this.center, seriesName, random.pickSeriesColor(), dataY, labelSlotsController);
-        var connectorInfo = spsv.getConnectorInfo();
+        let connectorInfo = spsv.getConnectorInfo();
 
         if(connectorInfo.isLeft){
             leftConnectorsInfo.push(connectorInfo);
@@ -48,21 +69,15 @@ BasicPieDataViewer.prototype.draw = function(){
         spsv.draw();
     }
 
-    //test start
-//    for(var i = 0; i < leftConnectorsInfo.length; i++){
-//        svgGroup.appendChild(draw.createCircle(leftConnectorsInfo[i].position[0], leftConnectorsInfo[i].position[1], 2));
-//    }
-    //test end
-
     if(!this.input.noDataLabel){
         leftConnectorsInfo = labelSlotsController.processConnectorInfo(leftConnectorsInfo, true);
         rightConnectorsInfo = labelSlotsController.processConnectorInfo(rightConnectorsInfo, false);
-        for(var i = 0; i < leftConnectorsInfo.length; i++){
+        for(let i = 0; i < leftConnectorsInfo.length; i++){
             svgGroup.appendChild(labelSlotsController.createConnectionLine(leftConnectorsInfo[i]));
             svgGroup.appendChild(labelSlotsController.createTextInSlot(leftConnectorsInfo[i].text, leftConnectorsInfo[i].slotIdx, true));
         }
 
-        for(var i = 0; i < rightConnectorsInfo.length; i++){
+        for(let i = 0; i < rightConnectorsInfo.length; i++){
             svgGroup.appendChild(labelSlotsController.createConnectionLine(rightConnectorsInfo[i]));
             svgGroup.appendChild(labelSlotsController.createTextInSlot(rightConnectorsInfo[i].text, rightConnectorsInfo[i].slotIdx, false));
         }
@@ -73,11 +88,11 @@ BasicPieDataViewer.prototype.draw = function(){
 };
 
 BasicPieDataViewer.prototype.analyze = function(){
-    var slices = []; //[startAngle, endAngle, seriesName, dataY, startAngle, endAngle, seriesName, dataY....]
-    var startAngle = 0;
-    for(var i = 0; i < this.input.series.length; i++){
-        var single = this.input.series[i][1];
-        var angle = (single / this.total) *  360;
+    let slices = []; //[startAngle, endAngle, seriesName, dataY, startAngle, endAngle, seriesName, dataY....]
+    let startAngle = 0;
+    for(let i = 0; i < this.input.series.length; i++){
+        let single = this.input.series[i][1];
+        let angle = (single / this.total) *  360;
 
         slices.push(startAngle); //start angle.
         slices.push(startAngle + angle); //end angle.
